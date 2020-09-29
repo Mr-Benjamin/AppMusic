0b50a142039bdb303d081d7a01f6f7791dd6b450
Mr-Benjamin <28747033+Mr-Benjamin@users.noreply.github.com>
2020-09-29 22:35:26 +1300

    Add project files.

---
+++ Models/Album.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMusic.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+
+        public string Title{ get; set; }
+
+        public string Year{ get; set; }
+
+        public int GenreId { get; set; }
+
+        public int MyArtist { get; set; }
+        public List<Artist> ArtistIds { get; set; }
+
+        public string MySong { get; set; }
+        public List<Song> Songs{ get; set; }
+
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppMusic.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongID { get; set; }
+        public string Title { get; set; }
+        public int AlbumId { get; set; }
+    }
+}
